8acd18539d4626cef6f41bf94f0f3731b0ebd3ac
Vansh Jaiswal <vjas2606@gmail.com>
2026-06-30 17:27:07 +0530

    Start of Vercel

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qapcl#$1d6wg%#gm(@189_e6@%mrw64-nj-zvnduukxyn&6uz!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
